b5f9ffc1ef1e7d442d33be03dc242f5aacc92e19
oudom <oudomphoem@gmail.com>
2026-02-09 03:43:09 +0700

    fix: add about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Layout from "@/components/layout";
 import Hero from "@/components/sections/hero";
+import About from "@/components/sections/about";
 import Skills from "@/components/sections/skills";
 import Projects from "@/components/sections/projects";
 import Contact from "@/components/sections/contact";
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <Layout>
       <Hero />
+      <About />
       <Skills />
       <Projects />
       <Contact />
